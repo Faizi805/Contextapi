8ac6826132ce9a15032274b1252a1429a6d58ec2
Faizi805 <faizishah805@gmail.com>
2024-04-02 02:03:37 +0500

    added files

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import UserContextProvider from './context/UserContextProvider'
+import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+      <h1>Faizan Shah</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
